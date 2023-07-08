30acf153ccdfd03c17f5ec661e54dc03e6dbdb71
NaorOrZion <naor5506@gmail.com>
2023-07-08 16:49:25 +0300

    change to different library - simple library to make sankey charts

--- sankey-stocks.py
+++ sankey-stocks.py
@@ -1,69 +1,24 @@
 import pandas as pd
-from floweaver import *
+import matplotlib.pyplot as plt
+from pySankey.sankey import sankey
 
-"""
-floWeaver uses different columns for different purposes based on their names.
-Specifically,
+df = pd.read_csv("WorldCruiseData.csv", sep=",")
 
-source:         specifies where the flow starts
-target:         specifies where the flow ends
-type:           specifies the type of the flow
-value:          specifies the flow rate
-
-Thus, we will rename some columns so that they fit floWeaver’s format.
-We will use:
-
-EMBARK_PORT:    as source
-DISEMBARK_PORT: as target
-CRUISE_REGION:  as type
-PRICE_PAID:     as value
-"""
-
-df = pd.read_csv("WorldCruiseData.csv")
-
-flows = (
-    df.groupby(["EMBARK_PORT", "DISEMBARK_PORT", "CRUISE_REGION"])
-    .agg({"PRICE_PAID": "mean"})
-    .dropna()
-    .reset_index()
+# Create Sankey diagram again
+sankey(
+    left=df["INCOME"], right=df["EXPENSES"], 
+    leftWeight= df["PRICE"], rightWeight=df["PRICE"], 
+    aspect=20, fontsize=20
 )
 
-flows = (
-    flows.rename(
-        columns={
-            "EMBARK_PORT": "source",
-            "DISEMBARK_PORT": "target",
-            "CRUISE_REGION": "type",
-            "PRICE_PAID": "value",
-        }
-    )
-)
+# Get current figure
+fig = plt.gcf()
 
-nodes = {
-    "embark_port": ProcessGroup(flows["source"].unique().tolist()),
-    "disembark_port": ProcessGroup(flows["target"].unique().tolist()),
-}
+# Set size in inches
+fig.set_size_inches(6, 6)
 
-ordering = [["embark_port"], ["disembark_port"]]
-bundles = [Bundle("embark_port", "disembark_port")]
-sdd = SankeyDefinition(nodes, bundles, ordering)
+# Set the color of the background to white
+fig.set_facecolor("w")
 
-# Create partitions for each type of port
-embark_port = Partition.Simple("process", flows["source"].unique().tolist())
-disembark_port = Partition.Simple("process", flows["target"].unique().tolist())
-
-nodes["embark_port"].partition = embark_port
-nodes["disembark_port"].partition = disembark_port
-
-"""
-In the diagram:
-
-The nodes on the left represent different embark ports.
-The nodes on the right represent different disembark ports.
-The line shows the travel path of the cruise from one port to another port.
-The thicker a line is, the more expensive the trip is.
-"""
-
-# Create Sankey diagram
-swidget = weave(sdd, flows, link_color=QuantitativeScale("value")).to_widget()
-swidget.auto_save_png(".png")
+# Save the figure
+fig.savefig("customers-goods.png", bbox_inches="tight", dpi=150)
